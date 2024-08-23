3ae7e74f3896a7e6ab7b823ec50d8f1507c39184
ramiro <ramiro@appelsoft.com.br>
2024-08-23 11:24:51 -0300

    tratamento valor zerado

--- src/Datagrid/Colunas/RadiantiDatagridColunaDinheiro.php
+++ src/Datagrid/Colunas/RadiantiDatagridColunaDinheiro.php
@@ -9,7 +9,10 @@ class RadiantiDatagridColunaDinheiro extends TDataGridColumn
     public function __construct($name, $label, $align = 'right', $width = NULL)
     {
         parent::__construct($name, $label, $align, $width);
+
         $this->setTransformer(function ($value) {
+            if (empty($value))
+                $value = 0;
             return 'R$ ' . number_format($value, 2, ',', '.');
         });
     }
