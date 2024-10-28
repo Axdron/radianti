4e58f353245b6639c8fc03df83182f7b8e7078f4
ramiro <ramiro@appelsoft.com.br>
2024-10-28 08:03:03 -0300

    melhoria na criação do elemento botaoSn

--- src/Componentes/RadiantiElementoBotaoSN.php
+++ src/Componentes/RadiantiElementoBotaoSN.php
@@ -7,9 +7,12 @@ use Axdron\Radianti\Utils\RadiantiDatasets;
 class RadiantiElementoBotaoSN extends RadiantiElementoBotaoOpcoes
 {
 
-    public function __construct(String $nome)
+    public function __construct(String $nome, int|string $opcaoPadrao = 1, bool $opcaoTodos = false)
     {
-        parent::__construct($nome, RadiantiDatasets::ARRAY_SN);
+        parent::__construct($nome, RadiantiDatasets::ARRAY_SN, $opcaoPadrao);
+        if ($opcaoTodos) {
+            $this->ativarOpcaoTodos();
+        }
     }
 
     function ativarOpcaoTodos()
