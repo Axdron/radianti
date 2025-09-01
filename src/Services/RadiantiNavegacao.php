572aecee2251e98b8c9552b99494e831dd34091e
ramiro <ramiro@appelsoft.com.br>
2025-09-01 17:51:57 -0300

    feat: melhoria na documentacao da Radianti navegação

--- src/Services/RadiantiNavegacao.php
+++ src/Services/RadiantiNavegacao.php
@@ -11,6 +11,12 @@ class RadiantiNavegacao
         AdiantiCoreApplication::loadPage($classe, $metodo, $parametros);
     }
 
+    /**
+     * Abre uma nova guia no navegador.
+     * @param string $classe - Prefira usar a declaração CLASSE::class do que uma string literal.
+     * @param string|null $method - Quando houver necessidade de parâmetros extras, recomenda-se informá-los aqui e não informar a key.
+     * @param mixed|null $key -  Algumas classes trabalham com key, outras com id. Para garantir que o comportamento seja consistente, a função irá informar ambos os valores.
+     */
     static function abrirNovaGuia(string $classe, ?string $method = null, $key = null)
     {
         $endereco = "index.php?class={$classe}";
